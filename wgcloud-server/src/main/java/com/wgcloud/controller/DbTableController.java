cfe37ef327d69a7a0b97fa8070dfeed65eeaca80
wgcloud <tianshiyeben@qq.com>
2022-08-20 11:15:44 +0800

    代码完善

--- wgcloud-server/src/main/java/com/wgcloud/controller/DbTableController.java
+++ wgcloud-server/src/main/java/com/wgcloud/controller/DbTableController.java
@@ -32,7 +32,7 @@ import java.util.Map;
  * @author: http://www.wgstart.com
  * @date: 2019年11月16日
  * @Description: DbTableController.java
- * @Copyright: 2017-2021 wgcloud. All rights reserved.
+ * @Copyright: 2017-2022 wgcloud. All rights reserved.
  */
 @Controller
 @RequestMapping("/dbTable")
